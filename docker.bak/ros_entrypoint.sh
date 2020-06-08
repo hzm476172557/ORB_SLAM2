aafc6f68074aa9c93ebc33ecd92c52bb77a27b28
yubaoliu <yubaoliu89@gmail.com>
2020-06-08 21:42:41 +0900

    Update docker

--- docker.bak/ros_entrypoint.sh
+++ docker.bak/ros_entrypoint.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-
-echo " Enrypoint"
 set -e
 
 # setup ros environment
 echo "ROS workspace: /root/catkin_ws"
 source "/opt/ros/melodic/setup.bash"
 source "/root/catkin_ws/devel/setup.bash"
-
-echo "----------------compile ORB SLAM2------------"
-cd /root/catkin_ws/src/ORB_SLAM2
-./build.sh
-./build_ros.sh
-
-cd /root/catkin_ws
-catkin_make
-
 exec "$@"
 
